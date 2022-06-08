6ee8a7068bdac539498e2b2efc037afe660a1622
Paul-Eirik <pmelhus@gmail.com>
2022-06-07 21:20:31 -0400

    started aws s3 image uploads. still need to create route and front end form.

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,33 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', bio='How about demo apples?', image_url="s3://dischord-profile-images/apple.jpeg")
+    vern = User(
+        username='Coolycool510', email='cool@cool.com', password='password', bio="I'm too cooly cool for you!", image_url=None)
+    bon = User(
+        username='BonJovi', email='bon@jovi.com', password='password', bio="It's my life!", image_url="s3://dischord-profile-images/bon.jpeg")
+    darren = User(
+        username='ComeEatChinaCity', email='china@city.com', password='password', bio="Ara smells!", image_url=None)
+    ara = User(
+        username='Zensan', email='zen@san.com', password='password', bio="NEZUKO-CHAAAAN!!!", image_url="s3://dischord-profile-images/zen.jpeg")
+    chris = User(
+        username='Push2Main', email='chris@thegrepper.com', password='password', bio="Nothing will get in the way of me and my taco bell... HELLO?!", image_url=None)
+    brianna = User(
+        username='AtlShawty', email='atl@shawty.com', password='password', bio="Go Hawks!", image_url=None)
+    brendan = User(
+        username='Week6', email='week@6.com', password='password', bio='WEEK 6?!?! HELLO?!?', image_url=None)
+    anthony = User(
+        username="TheBroncaNator", email='bronca@bronca.com', password='password', bio="Ara ara...", image_url=None)
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(vern)
+    db.session.add(bon)
+    db.session.add(darren)
+    db.session.add(ara)
+    db.session.add(chris)
+    db.session.add(brianna)
+    db.session.add(brendan)
+    db.session.add(anthony)
 
     db.session.commit()
 

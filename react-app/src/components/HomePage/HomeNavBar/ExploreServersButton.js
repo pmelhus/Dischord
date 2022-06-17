2b20f61b6d51afe3219ccc3d1f3eada616d097d9
Paul-Eirik <pmelhus@gmail.com>
2022-06-17 11:14:11 -0400

    css edits

--- react-app/src/components/HomePage/HomeNavBar/ExploreServersButton.js
+++ react-app/src/components/HomePage/HomeNavBar/ExploreServersButton.js
@@ -1,9 +1,9 @@
 const ExploreServersButton = () => {
   return (
     <div className='home-button-div'>
-      <button className="server-image-icon" >
+      {/* <button className="server-image-icon" >
       <i className="fa-brands fa-wpexplorer fa-2xl"></i>
-      </button>
+      </button> */}
     </div>
   );
 };

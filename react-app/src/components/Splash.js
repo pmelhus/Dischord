ed2700db12db34e70a9d5b957a10354b5ec8172e
Paul-Eirik <pmelhus@gmail.com>
2022-06-20 01:45:53 -0400

    Revert "finished up css on splash"
    This reverts commit aebb348a0bfd629d447430bb7b677e29dba2633c.

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -1,40 +1,30 @@
 // import  from "../images/DISCHORD.png"
 
 const Splash = () => {
+
   return (
-    <div className="splash-container">
+    <div>
       <div className="splash-text-rel">
-        <div className="splash-text-div">
-          <h1>Imagine a place...</h1>
-          <p>
-            ... where you can connect with like-minded, passionate artists,
-            share musical concepts or ideas with multiple peers at once, and
-            have a space to play music together, remotely.
-          </p>
-        </div>
+
+      <div className="splash-text-div">
+        <h1>Imagine a place...</h1>
+        <p>
+          ... where you can connect with like-minded, passionate artists, share
+          musical concepts or ideas with multiple peers at once, and have a
+          space to play music together, remotely.
+        </p>
+      </div>
       </div>
       <video id="video-dj" autoPlay muted loop>
         <source src="https://res.cloudinary.com/dmtap2h65/video/upload/v1655278059/mixkit-dj-mixing-acetate-discs-4127_pz6tdg.mp4"></source>
       </video>
-      <div className="splash-violin-div">
+      <div className='splash-violin-div'>
         <video autoPlay muted loop id="video-violin">
           <source src="https://res.cloudinary.com/dmtap2h65/video/upload/v1655278935/AdobeStock_371646570_reduced_dmnv3q.mov"></source>
         </video>
-        <h2>Create an invite-only place for your musical friends</h2>
-      </div>
-      <div className="splash-band-div">
-        <h2>Where hanging out is easy</h2>
-
-        <video autoPlay muted loop id="video-band">
-          <source src="https://res.cloudinary.com/dmtap2h65/video/upload/v1655278605/mixkit-amateur-band-playing-rock-3272_wkzxnh.mp4"></source>
-        </video>
-      </div>
-      <div className="splash-band2-div">
-
-        <video autoPlay muted loop id="video-band2">
-          <source src="https://res.cloudinary.com/dmtap2h65/video/upload/v1655701784/AdobeStock_215440730_en1cdu.mov"></source>
-        </video>
-        <h2>Network and meet new musicians</h2>
+        <h2>
+          Create an invite-only place for your musical friends
+        </h2>
       </div>
     </div>
   );
